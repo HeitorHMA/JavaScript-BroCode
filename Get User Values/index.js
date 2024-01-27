8f188660aa449ee59574c1525632bba4abebd06f
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-27 17:37:02 -0300

    Learned

--- Get User Values/index.js
+++ Get User Values/index.js
@@ -4,3 +4,6 @@ document.getElementById("submitName").onclick = function(){
    userName = document.getElementById("userText").value;
    document.getElementById("myH1").textContent = `Hello ${userName}`
 }
+
+let usuarioNome
+usuarioNome = window.prompt("Qual seu nome?")
---
+++ Type-Conversion/index.js
@@ -0,0 +1,10 @@
+let x = window.prompt("Write here an string")
+alert(`${x}, is an ${typeof x}`)
+
+let y = window.prompt("Write here an number")
+y = number(10)
+console.log(`${y}, is an ${typeof y}`)
+
+let z = window.prompt("Write here an boolean")
+z = boolean(z)
+console.log(`${z}, is an ${typeof z}`)

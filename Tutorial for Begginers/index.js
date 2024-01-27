4accbd1dc079b4c2c92f59c2bfae2d341bb001b9
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-26 22:27:36 -0300

    Finish Lesson

---
+++ Tutorial for Begginers/index.js
@@ -0,0 +1,13 @@
+console.log(`Hello`);
+console.log("I like pizza");
+
+
+document.getElementById("myH1").textContent = `Hello Guys`;
+document.getElementById("myP").textContent = "I like Pizza"
+
+
+window.alert(`This is an alert`)
+window.alert(`I like Pizza`)
+
+// This is a comment
+

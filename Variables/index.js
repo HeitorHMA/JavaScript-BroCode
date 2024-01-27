7a1b44e8d7618c413e11b29b348674f155b15ff1
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-27 06:48:34 -0300

    Second Lesson

---
+++ Variables/index.js
@@ -0,0 +1,11 @@
+let fullName;
+let age;
+let goodAtCoding;
+
+fullName = "Heitor Henrique";
+age = "18";
+goodAtCoding = true;
+
+document.getElementById("p1").textContent = (`My full name is ${fullName}`);
+document.getElementById("p2").textContent = (`I'm ${age} years old`)
+document.getElementById("p3").textContent = (`Good at conding: ${goodAtCoding}`)

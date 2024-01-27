fb47cb2e18f384694ffc43211dc2e43b28624a33
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-27 17:46:07 -0300

    Learn lesson.

---
+++ Circuference-Calculator/index.js
@@ -0,0 +1,5 @@
+const pi = 3.14
+let radius = window.prompt("Write an radius")
+radius = Number(radius)
+let circumference = 2 * pi * radius 
+alert(`The circumference of that radius is ${circumference}`)
--- Type-Conversion/index.js
+++ Type-Conversion/index.js
@@ -2,9 +2,9 @@ let x = window.prompt("Write here an string")
 alert(`${x}, is an ${typeof x}`)
 
 let y = window.prompt("Write here an number")
-y = number(10)
-console.log(`${y}, is an ${typeof y}`)
+y = Number(10)
+alert(`${y}, is an ${typeof y}`)
 
 let z = window.prompt("Write here an boolean")
-z = boolean(z)
-console.log(`${z}, is an ${typeof z}`)
+z = Boolean(z)
+alert(`${z}, is an ${typeof z}`)

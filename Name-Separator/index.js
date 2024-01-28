015982c91d8348cbd5722233dd80948d1b920829
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-28 17:15:04 -0300

    learn slicing

---
+++ Name-Separator/index.js
@@ -0,0 +1,5 @@
+let fullName= "Heitor Henrique"
+let firstName = fullName.slice(0, fullName.indexOf(" "))
+let lastName = fullName.slice(fullName.indexOf(" "))
+console.log (firstName)
+console.log (lastName)

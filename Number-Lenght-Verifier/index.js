b7d6d3a6e6ebc083ee7b9df8b0a12925f8594c37
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-28 16:53:00 -0300

    learn

---
+++ Number-Lenght-Verifier/index.js
@@ -0,0 +1,9 @@
+let userNumber = "10945678"
+let numberLenght = userNumber.length
+if (numberLenght >= 9){
+    console.log("Your number can't be bigger than 8 digits")
+}
+else {
+    console.log(`Your number is ${userNumber} , right? `)
+}
+console.log(numberLenght)

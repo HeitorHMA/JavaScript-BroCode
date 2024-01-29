b16350b35e48147b58c6f9e706da4eccd554df2a
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-29 13:57:31 -0300

    learn

---
+++ Day-2/Spread-Operators/index.js
@@ -0,0 +1,4 @@
+let fruits = ["bananas", "orange", "watermellon" , "jackfruit" , "dragonfruit"]
+let superheroes = ["batman", "robin" , "batman again"]
+let newFruits = [...fruits,...superheroes]
+alert (newFruits)

1371034ab3cab8bb3b4ef3a031de6713d753c540
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-29 10:11:57 -0300

    Learn

---
+++ Day-2/Global-vs-Local/index.js
@@ -0,0 +1,16 @@
+let result = 100
+console.log(result)
+
+function add (x, y) {
+    let result = x + y
+    console.log(result)
+}
+
+add(2, 5)
+
+subtract( 10, 5)
+function subtract(x,y) {
+    let result = x - y
+    console.log(result)
+
+}
